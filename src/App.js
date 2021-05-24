c814a98dc6efd2ef6efb434189f9a8f74ced6929
Diana L Munoz <dlmp7895@gmail.com>
2021-05-24 07:38:21 -0400

    Added the GitHub

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      My weather App
+      <footer>  My weather App was coded by <a href="https://github.com/dilamu">Diana L Munoz</a>, {""}
+      <a href="https://github.com/dilamu/weather-app-react" target="_blank"> open source</a>
+       </footer>  
     </div>
   );
 }
