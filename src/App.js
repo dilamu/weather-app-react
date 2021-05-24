9c89554b2b6abba607614dc58c3155815625906d
Diana L Munoz <dlmp7895@gmail.com>
2021-05-24 08:09:21 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <footer>  My weather App was coded by <a href="https://github.com/dilamu">Diana L Munoz</a>, {""}
-      <a href="https://github.com/dilamu/weather-app-react" target="_blank"> open source</a>
-       </footer>  
+      <div className="container">
+      <h1>Weather App</h1>
+        <footer>  My weather App was coded by <a href="https://github.com/dilamu">Diana L Munoz</a>, {""}
+        <a href="https://github.com/dilamu/weather-app-react" target="_blank" rel="noreferrer"> open source on GitHub</a>
+        </footer> 
+        </div> 
     </div>
   );
 }
