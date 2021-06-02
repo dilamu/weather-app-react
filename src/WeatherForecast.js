3767b15b2b02ead9861db7b986d96a836ffd7ae1
Diana L Munoz <dlmp7895@gmail.com>
2021-06-01 19:27:22 -0500

    updated weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
-import { cleanup } from "@testing-library/react";
 
 
 export default function WeatherForecast(props) {  
